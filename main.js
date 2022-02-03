991ae1cec52c5c34781d5d2d6e197e63e9ddafbc
InsherahAL <juju.abulibdeh@gmail.com>
2022-02-03 15:19:31 +0200

    dding files

---
+++ main.js
@@ -0,0 +1,36 @@
+// const c =19
+// const f = c * 1.8 + 32
+// console.log(f)
+// let num = 1
+// let lessThanOne = num = 0
+// let result = " "
+// if (lessThanOne) {
+//     result = ` you have  ${num} sandwich`
+    
+// } else {
+
+//     result = `you have ${num}  sandwiches`
+// }
+// console.log(result)
+let mark = "68"
+let result = ' '
+
+
+if (mark>=90) {
+    result =`${mark} excelent`
+    
+} else  if (mark>=80) {
+    result =`${mark} very good`
+    
+} else if (mark>=70) {
+    result =`${mark} good`
+    
+} else if (mark>=60) {
+    result =`${mark} fair`
+} else {
+    result =`${mark} failed`
+}
+    
+
+
+console.log(result)
